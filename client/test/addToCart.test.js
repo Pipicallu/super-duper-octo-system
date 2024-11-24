dc2f658aea10838db86cd13863a57c2671b37088
Tumas Vassallo <tumas.vassallo@gmail.com>
2024-11-24 15:08:40 +0000

    added additional test to check for multiple items

--- client/test/addToCart.test.js
+++ client/test/addToCart.test.js
@@ -13,3 +13,13 @@ test("updates the quantity and price of an existing product in the basket", () =
   const updatedBasket = addToCart(basket, product);
   expect(updatedBasket).toEqual([{ id: 1, name: "Product 1", quantity: 2, price: 200 }]);
 });
+
+test("shows multiple products in the basket", () => {
+  const basket = [{ id: 1, name: "Product 1", quantity: 1, price: 100 }];
+  const product = { id: 2, name: "Product 2", quantity: 1, price: 200 };
+  const updatedBasket = addToCart(basket, product);
+  expect(updatedBasket).toEqual([
+    { id: 1, name: "Product 1", quantity: 1, price: 100 },
+    { id: 2, name: "Product 2", quantity: 1, price: 200 },
+  ]);
+});
