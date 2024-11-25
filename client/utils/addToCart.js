3532d790ff632256d6b21fb3a9dd64e9d5578e2c
Tumas Vassallo <tumas.vassallo@gmail.com>
2024-11-25 08:37:05 +0000

    refactored add to cart function to make more readable

--- client/utils/addToCart.js
+++ client/utils/addToCart.js
@@ -1,15 +1,21 @@
 export const addToCart = (basket, product) => {
-    if (basket.find((item) => item.id === product.id)) {
-      return basket.map((item) => {
-        if (item.id === product.id) {
-          return {
-            ...item,
-            quantity: item.quantity + product.quantity,
-            price: item.price + product.price,
-          };
-        }
-        return item;
-      });
+  let productExists = false;
+
+  const updatedBasket = basket.map((item) => {
+    if (item.id === product.id) {
+      productExists = true;
+      return {
+        ...item,
+        quantity: item.quantity + product.quantity,
+        price: item.price + product.price,
+      };
     }
-    return [...basket, product];
-  };
+    return item;
+  });
+
+  if (!productExists) {
+    return [...updatedBasket, product];
+  }
+
+  return updatedBasket;
+};
